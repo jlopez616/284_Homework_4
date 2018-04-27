1afa62ad309ad309bbe4a14b80932905c25acedc
John Lopez <johnllopez616@gmail.com>
2018-04-26 19:56:52 -0700

    Added actual content

--- input.c
+++ input.c
@@ -1,34 +1,55 @@
 #include <stdio.h>
-#include <ctype.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int returnCode = 0;
+
+static bool legal(uint32_t codepoint) {
+    return codepoint <= 0x0010FFFF
+        && (codepoint < 0x0000FDD0 || codepoint > 0x0000FDEF)
+        && (codepoint & 0x0000FFFE) != 0x0000FFFE;
+      }
+
+void convert(int block) {
+  if (!legal(block)) {
+    returnCode = 1;
+    putchar(0xEF);
+    putchar(0xBF);
+    putchar(0xBB);
+  } else if (block <= 0x7F) {
+       putchar(block);
+   } else if (block <= 0x7FF) {
+       putchar(0xC0 | block>>6);
+       putchar(0x80 | block & 0x3F);
+   } else if (block <= 0xFFFF) {
+       putchar(0xE0 | block>>12);
+       putchar(0x80 | block>>6 & 0x3F);
+       putchar(0x80 | block & 0x3F);
+   } else {
+       putchar(0xF0 | block>>18);
+       putchar(0x80 | block>>12 & 0x3F);
+       putchar(0x80 | block>>6 & 0x3F);
+       putchar(0x80 | block & 0x3F);
+   }
+
+}
 
 
 int main() {
-  const int blockSize = 1024;
-  char buffer[blockSize];
 
-  while (blockSize != EOF) {
-    int bytes_read = fread(buffer, blockSize, sizeof(w_char_t), stdin); //everything read will go into here
+  const int blockSize = 1024;
+  uint32_t buffer[blockSize];
+
+  while (true) {
+    int bytes_read = fread(buffer, sizeof(uint32_t), blockSize, stdin); //everything read will go into here
     if (bytes_read == 0) {
       break;
     }
-/*
 
-ACTUAL CONTENT WILL GO here
-*/
-
-
-    /*for (int i = 0; i < bytes_read; i++) {
-      buffer[i]= toupper(buffer[i]);
-    } */
-
-    fwrite(buffer, sizeof(w_char_t), bytes_read, stdout);
-
-    //for e', the codepoint is U+00E9
-    //the 11 000 11 1 0001
-    //to add a 10 to a codepoint, OR 1000000 with it, and the first two bits will fall off.
-
-    //use the right shift operator (>> to move
-
-
+    for (int k = 0; k < blockSize; k++) {
+      convert(buffer[k]);
+    }
   }
+
+  return returnCode;
 }
