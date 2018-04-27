dee09c4d5db6a3bdfe2457a3875dfd686afcff57
Ray Toal <rtoal@lmu.edu>
2018-04-26 20:05:45 -0700

    Bugfix

--- input.c
+++ input.c
@@ -20,16 +20,16 @@ void convert(int block) {
        putchar(block);
    } else if (block <= 0x7FF) {
        putchar(0xC0 | block>>6);
-       putchar(0x80 | block & 0x3F);
+       putchar(0x80 | (block & 0x3F));
    } else if (block <= 0xFFFF) {
        putchar(0xE0 | block>>12);
-       putchar(0x80 | block>>6 & 0x3F);
-       putchar(0x80 | block & 0x3F);
+       putchar(0x80 | (block>>6 & 0x3F));
+       putchar(0x80 | (block & 0x3F));
    } else {
        putchar(0xF0 | block>>18);
-       putchar(0x80 | block>>12 & 0x3F);
-       putchar(0x80 | block>>6 & 0x3F);
-       putchar(0x80 | block & 0x3F);
+       putchar(0x80 | (block>>12 & 0x3F));
+       putchar(0x80 | (block>>6 & 0x3F));
+       putchar(0x80 | (block & 0x3F));
    }
 
 }
@@ -41,12 +41,12 @@ int main() {
   uint32_t buffer[blockSize];
 
   while (true) {
-    int bytes_read = fread(buffer, sizeof(uint32_t), blockSize, stdin); //everything read will go into here
+    int bytes_read = fread(buffer, sizeof(uint32_t), blockSize, stdin);
     if (bytes_read == 0) {
       break;
     }
 
-    for (int k = 0; k < blockSize; k++) {
+    for (int k = 0; k < bytes_read; k++) {
       convert(buffer[k]);
     }
   }
